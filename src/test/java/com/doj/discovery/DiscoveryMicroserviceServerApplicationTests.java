8140fc2767b7b51afd045fbb1be183969aef9007
saranrajv <saranraj.venkatesan@olamnet.com>
2019-06-20 15:35:13 +0530

    initial commit

---
+++ src/test/java/com/doj/discovery/DiscoveryMicroserviceServerApplicationTests.java
@@ -0,0 +1,19 @@
+package com.doj.discovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryMicroserviceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println(fdsfa);
+		System.out.println(xyadfsd);
+		System.out.println(xyz);
+	}
+
+}
